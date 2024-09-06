e19c3890435e31afe3c846795a6803bc104c1b30
QEDEKgatla <139855907+QEDEKgatla@users.noreply.github.com>
2024-09-06 21:17:16 +0200

    Update flasksever.py

--- New folder/flasksever.py
+++ New folder/flasksever.py
@@ -1,23 +1,16 @@
-from flask import Flask, render_template, jsonify
-import subprocess
-import os
+from flask import Flask, send_from_directory
 
-# Create the Flask app and specify the custom template folder location
-app = Flask(__name__, template_folder=r'New folder')
+app = Flask(__name__, static_folder=r'C:\Users\ElphusK\Videos\New Folder\New folder\static')
 
+# Route to serve static files
+@app.route('/copperlichtdata/<path:filename>')
+def serve_static(filename):
+    return send_from_directory(r'C:\Users\ElphusK\Videos\New Folder\New folder\static\copperlichtdata', filename)
+
+# Route to serve the HTML file
 @app.route('/')
 def index():
-    return render_template('index.html')  # Renders index.html from the specified folder
+    return send_from_directory(r'C:\Users\ElphusK\Videos\New Folder\New folder\static', 'T.html')
 
-@app.route('/run-game', methods=['POST'])
-def run_game():
-    try:
-        # Run the executable file with the correct path
-        exe_path = r'New folder\myGame.exe'
-        subprocess.run([exe_path], check=True)
-        return jsonify({"status": "success", "message": "Game executed successfully."})
-    except subprocess.CalledProcessError as e:
-        return jsonify({"status": "error", "message": str(e)})
-
-if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+if __name__ == '__main__':
+    app.run(port=8000)
